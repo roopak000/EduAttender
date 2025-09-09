dc26fb7d61fa7a0691ff8464f2ce1e21afd1f96d
roopak000 <roopaksharma000@gmail.com>
2025-09-09 06:21:39 +0530

    Initial Commit

--- src/main/java/com/tka/sams/api/EduAttenderApplication.java
+++ src/main/java/com/tka/sams/api/EduAttenderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SamsTrackApplication {
+public class EduAttenderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SamsTrackApplication.class, args);
+		SpringApplication.run(EduAttenderApplication.class, args);
 	}
 
 }
